97403085c6afe81d82adb8c86ad37bd2d80aaea9
arkain123 <dimabatkabatka14@gmail.com>
2024-03-03 03:36:47 +0300

    renamed pythonProject1 -> Infection-Bot

---
+++ Infection-Bot/controller/commands.py
@@ -0,0 +1,24 @@
+import disnake
+import view.console_out as out
+
+#@main.bot.command()
+#async def hello(ctx):
+#    await ctx.reply("world")
+#    out.log(f"{ctx.author.mention} used /hello")
+
+#@main.bot.command()
+#async def embedtest(ctx):
+#    embed = disnake.Embed(
+#        title=f"Embed title",
+#        description=f"description",
+#        color=0xfffff
+#    )
+#    await ctx.send(embed=embed)
+#    out.log(f"{ctx.author.mention} used /embedtest")
+
+#@main.bot.command()
+#async def test(ctx):
+#    out.log(f"{ctx.author.mention} used /test")
+#    out.log(f"{ctx.author.mention} log message")
+#    out.warning(f"{ctx.author.mention} warning message")
+#    out.error(f"{ctx.author.mention} error message")
---
+++ Infection-Bot/controller/events.py
@@ -0,0 +1,6 @@
+import disnake
+import view.console_out as out
+
+#@main.bot.event
+#async def on_ready():
+#    out.log("Bot started, ready to work!")
