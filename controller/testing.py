d0ba841d22646350b8d2317d2aae7e64a8b78d30
arkain123 <dimabatkabatka14@gmail.com>
2024-03-18 00:08:57 +0300

    changed command to slash_command

--- controller/testing.py
+++ controller/testing.py
@@ -10,7 +10,7 @@ class Testing(commands.Cog):
         self.bot = bot
         important("COGS module Testing connected")
 
-    @commands.command()
+    @commands.slash_command()
     async def embedtest(self, ctx):
         view = Button()
         embed = disnake.Embed(
