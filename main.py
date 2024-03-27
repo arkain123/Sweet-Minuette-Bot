12ad22a13abe477df2b53c166a2f7c079e50095a
arkain123 <dimabatkabatka14@gmail.com>
2024-03-27 05:03:54 +0300

    getting ready for beta-1 release

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import view.console_out
 import random
 from dotenv import load_dotenv
 import os
-from controller import debug, botcommands, events, testing, Sosat, eastereggs
+from controller import debug, botcommands, events, testing, eastereggs
 from view import classes, console_out
 
 
@@ -17,7 +17,7 @@ def connect(module):
         return 0
 
 
-nummodules = 8
+nummodules = 4
 load_dotenv()
 PREFIX = '/'
 intents = disnake.Intents().all()
@@ -27,13 +27,12 @@ connections = 0
 bot = commands.Bot(command_prefix=PREFIX, help_command=None, intents=intents, test_guilds=[1175855563444330637, 715142906658422796])
 
 connections += connect("controller.botcommands")
-connections += connect("controller.debug")
-connections += connect("controller.Sosat")
+# connections += connect("controller.debug")
 # connections += connect("controller.events")
-connections += connect("controller.testing")
+# connections += connect("controller.testing")
 connections += connect("controller.mafia")
 connections += connect("controller.eastereggs")
-connections += classes.connected()
+# connections += classes.connected()
 connections += console_out.connected()
 
 # Number of successful module connections
